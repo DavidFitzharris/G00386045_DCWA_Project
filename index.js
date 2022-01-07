cef88a22dacf43ffe4fa6fa2e8d4e598e7ba3975
David Fitzharris <g00386045@gmit.ie>
2022-01-07 00:27:10 +0000

    StudentMID display

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var mongoDAO = require('./mongoDAO');
 // const mysql = require('promise-mysql')
 var sqlDAO = require('./sqlDAO');
+var bp = require('body-parser');
+//express validator ref https://express-validator.github.io/docs
+//https://express-validator.github.io/docs/5.3.1/check-api.html
+const { body, validationResult } = require('express-validator');
 
 var app = express();
 var port = 4000;
@@ -32,46 +36,99 @@ app.listen(port, () => {
 app.set('view engine', 'ejs');
 
 //To home page
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/views/home.html");
 })
 
+//****************************Modules*******************//
 //Modules view
 app.get('/listModules', (req, res) => {
-	//res.sendFile(__dirname + "/views/modules.ejs");
-	
 	sqlDAO.getModules()
+		.then((result) => {
+			// console.log(result)
+			res.render("modules", { modules: result })
+		})
+		.catch((error) => {
+			res.send(error)
+		})
+})
+
+app.get('/modules/edit/:mid', (req, res) => {
+	//Get by ID selected
+	//res.render("editModules")
+	sqlDAO.getModID(req.params.mid)
+		.then((result) => {
+			if (result != null) {
+				res.render("editModule", { mid: result[0].mid, name: result[0].name, credits: result[0].credits })
+				console.log(result);
+			} else {
+				res.send("ID Invalid")
+			}
+		})
+		.catch((error) => {
+			res.send(error)
+		})
+})
+
+//Update Module by ID
+//Currently not updating
+app.post('/modules/edit/:mid',
+	// body('name').isLength({min:5}),
+	// body('credit').isIn([5, 10, 15]),
+	(req, res) => {
+		//Post update of module, only changing the name or credits
+		let valCheck = validationResult(req);
+		if (valCheck.isEmpty()) {
+			sqlDAO.setModule(req.params.mid, req.body.name, req.body.credits)
+				.then((result) => {
+					console.log(result)
+					console.log("No Errors")
+					res.render("editModule", { mid: req.params.mid, name: req.body.name, credits: req.body.credits})
+				})
+				.catch((error) => {
+					res.send(error)
+					console.log("Edit Error")
+				})
+		} else 
+		//Reload 
+			res.render("editModule", { mid: req.params.mid, name: req.body.name, credits: req.body.credits})
+	})
+
+app.get('/modules/students/:mid', (req, res) => {
+	sqlDAO.getStudentByMID(req.params.mid)
 	.then((result) => {
-		console.log(result)
-		res.render("modules", { modules: result })
+		res.render("studentsMID", { students: result, mid: req.params.mid })
 	})
 	.catch((error) => {
 		res.send(error)
 	})
 })
 
+
+//****************************Lecturers*******************//
 //lecturers view
-app.get('/listLecturers', (req,res) => {
+app.get('/listLecturers', (req, res) => {
 	//res.sendFile(__dirname + "/views/lecturers.ejs");
 	mongoDAO.getLecturers()
-	.then((result) => {
-		console.log(result)
-		res.render("lecturers", { lecturers: result })
-	})
-	.catch((error) => {
-		res.send(error)
-	})
+		.then((result) => {
+			console.log(result)
+			res.render("lecturers", { lecturers: result })
+		})
+		.catch((error) => {
+			res.send(error)
+		})
 })
 
+//****************************Students*******************//
 //lecturers view
-app.get('/listStudents', (req,res) => {
+app.get('/listStudents', (req, res) => {
 	//res.sendFile(__dirname + "/views/students.ejs");
 	sqlDAO.getStudents()
-	.then((result) => {
-		console.log(result)
-		res.render("students", { students: result })
-	})
-	.catch((error) => {
-		res.send(error)
-	})
+		.then((result) => {
+			console.log(result)
+			res.render("students", { students: result })
+		})
+		.catch((error) => {
+			res.send(error)
+		})
 })
--- sqlDAO.js
+++ sqlDAO.js
@@ -10,33 +10,85 @@ mysql.createPool({
 	password: '',
 	database: 'collegeDB',
 })
-.then((result) => {
-	pool = result;
-})
-.catch((error) => {
-	console.log(error)
-})
+	.then((result) => {
+		pool = result;
+	})
+	.catch((error) => {
+		console.log(error)
+	})
 
 //get modules from collegedb
 var getModules = () => {
 	return new Promise((resolve, reject) => {
 		pool.query('select * from module')
-		.then((result) => {
-            console.log("Getting module table..")
-			resolve(result)
-		})
-		.catch((error) => {
-			reject(error)
-		})
+			.then((result) => {
+				console.log("Getting module table..")
+				resolve(result)
+			})
+			.catch((error) => {
+				reject(error)
+			})
 	})
 }
+
+//Get by Mod ID
+var getModID = (mid) => {
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "select * from module where mid = ?",
+			values: [mid]
+		}
+		pool.query(query)
+			.then((result) => {
+				resolve(result)
+			})
+			.catch((error) => {
+				reject(error)
+			})
+	})
+}
+
+//Set new name and credits
+var setModule = (mid, name, credits) => {
+	console.log("setModule called")
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "update module set mid = ?, credits = ? where name = ?",
+			values: [mid, credits, name]
+		}
+		pool.query(query)
+			.then((result) => {
+				resolve(result)
+			})
+			.catch((error) => {
+				reject(error)
+			})
+	})
+}
+
 
 //Get students from collegedb
 var getStudents = () => {
 	return new Promise((resolve, reject) => {
 		pool.query('select * from student')
+			.then((result) => {
+				//console.log("Getting student table..")
+				resolve(result)
+			})
+			.catch((error) => {
+				reject(error)
+			})
+	})
+}
+
+var getStudentByMID = (mid) => {
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "select * from student s left join student_module m on s.sid = m.sid where m.mid = ?",
+			values: [mid]
+		} 
+		pool.query(query)
 		.then((result) => {
-            console.log("Getting student table..")
 			resolve(result)
 		})
 		.catch((error) => {
@@ -45,4 +97,4 @@ var getStudents = () => {
 	})
 }
 
-module.exports = {getModules,getStudents};
+module.exports = { getModules, getStudents, getModID, setModule, getStudentByMID };
